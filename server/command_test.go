c55f568132f2ee2902b36dd8678f72613642022c
iomodo <wineson@gmail.com>
2019-06-25 21:11:16 +0400

    Add command tests

--- server/command_test.go
+++ server/command_test.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/mattermost/mattermost-server/model"
+	"github.com/mattermost/mattermost-server/plugin/plugintest"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
 // func assertThat(command, text string, )
@@ -42,22 +45,61 @@ func TestExecuteCommandTrivial(t *testing.T) {
 	assert.Contains(resp.Text, desc)
 }
 
-// func TestExecuteCommandSuggestChannels(t *testing.T) {
-// 	assert := assert.New(t)
-// 	plugin := Plugin{}
-// 	plugint := plugintest
-// 	api := &plugintest.API{}
-// 	api.On("RegisterCommand", mock.Anything).Return(nil)
-// 	api.On("UnregisterCommand", mock.Anything, mock.Anything).Return(nil)
-// 	api.On("GetUser", mock.Anything).Return(&model.User{
-// 		Id:       "userid",
-// 		Nickname: "User",
-// 	}, (*model.AppError)(nil))
+func TestExecuteCommandSuggestChannelsZero(t *testing.T) {
+	assert := assert.New(t)
+	plugin := Plugin{}
+	api := &plugintest.API{}
 
-// 	args := &model.CommandArgs{
-// 		Command: "",
-// 	}
-// 	resp, err := plugin.ExecuteCommand(nil, args)
-// 	assert.Nil(err)
-// 	assert.Nil(resp)
-// }
+	channelsZero := make([]*recommendedChannel, 0)
+	bytes, _ := json.Marshal(channelsZero)
+	api.On("KVGet", mock.Anything).Return(bytes, (*model.AppError)(nil))
+	plugin.SetAPI(api)
+
+	args := &model.CommandArgs{
+		Command: "/suggest channels",
+	}
+	resp, err := plugin.ExecuteCommand(nil, args)
+	assert.Nil(err)
+	assert.Equal(noNewChannels, resp.Text)
+}
+
+func TestExecuteCommandSuggestChannels(t *testing.T) {
+	assert := assert.New(t)
+	plugin := Plugin{}
+	api := &plugintest.API{}
+
+	channels := make([]*recommendedChannel, 1)
+	channels[0] = &recommendedChannel{ChannelID: "chan", Score: 0.1}
+	bytes, _ := json.Marshal(channels)
+
+	api.On("KVGet", mock.Anything).Return(bytes, (*model.AppError)(nil))
+	api.On("GetChannel", mock.Anything).Return(&model.Channel{DisplayName: "CoolChannel"}, model.NewAppError("", "", nil, "", 404))
+	plugin.SetAPI(api)
+
+	args := &model.CommandArgs{
+		Command: "/suggest channels",
+	}
+	_, err := plugin.ExecuteCommand(nil, args)
+	assert.NotNil(err)
+}
+
+func TestExecuteCommandSuggestChannelError(t *testing.T) {
+	assert := assert.New(t)
+	plugin := Plugin{}
+	api := &plugintest.API{}
+
+	channels := make([]*recommendedChannel, 1)
+	channels[0] = &recommendedChannel{ChannelID: "chan", Score: 0.1}
+	bytes, _ := json.Marshal(channels)
+
+	api.On("KVGet", mock.Anything).Return(bytes, (*model.AppError)(nil))
+	api.On("GetChannel", mock.Anything).Return(&model.Channel{DisplayName: "CoolChannel"}, (*model.AppError)(nil))
+	plugin.SetAPI(api)
+
+	args := &model.CommandArgs{
+		Command: "/suggest channels",
+	}
+	resp, err := plugin.ExecuteCommand(nil, args)
+	assert.Nil(err)
+	assert.Equal(" * Score 0.10 : CoolChannel \n", resp.Text)
+}
