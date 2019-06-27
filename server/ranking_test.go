83b430664325ee2c170aeb540e6ccb6abf131fff
iomodo <wineson@gmail.com>
2019-06-27 15:53:04 +0400

    Refactor ranking tests

--- server/ranking_test.go
+++ server/ranking_test.go
@@ -8,42 +8,65 @@ import (
 
 func TestRankingUnion(t *testing.T) {
 	assert := assert.New(t)
+	t.Run("Add new ranks", func(t *testing.T) {
+		r1 := userChannelRank{
+			"user1": {
+				"channel1": 100,
+				"channel2": 200,
+			},
+		}
+		r2 := userChannelRank{
+			"user2": {
+				"channel1": 100,
+				"channel2": 200,
+			},
+		}
+		rankingUnion(r1, r2)
+		res := userChannelRank{
+			"user1": {
+				"channel1": 100,
+				"channel2": 200,
+			},
+			"user2": {
+				"channel1": 100,
+				"channel2": 200,
+			},
+		}
+		assert.Equal(res, r1)
+	})
 
-	r1 := userChannelRank{
-		"user1": {
-			"channel1": 100,
-			"channel2": 200,
-		},
-	}
-	r2 := userChannelRank{
-		"user2": {
-			"channel1": 100,
-			"channel2": 200,
-		},
-	}
-	rankingUnion(r1, r2)
-	res1 := userChannelRank{
-		"user1": {
-			"channel1": 100,
-			"channel2": 200,
-		},
-		"user2": {
-			"channel1": 100,
-			"channel2": 200,
-		},
-	}
-	assert.Equal(res1, r1)
+	t.Run("Mix ranks", func(t *testing.T) {
+		r1 := userChannelRank{
+			"user1": {
+				"channel1": 100,
+				"channel2": 200,
+			},
+			"user2": {
+				"channel1": 100,
+				"channel2": 200,
+			},
+		}
+		r2 := userChannelRank{
+			"user2": {
+				"channel1": 100,
+				"channel2": 200,
+			},
+		}
+		rankingUnion(r1, r2)
+		res := userChannelRank{
+			"user1": {
+				"channel1": 100,
+				"channel2": 200,
+			},
+			"user2": {
+				"channel1": 200,
+				"channel2": 400,
+			},
+		}
+		assert.Equal(res, r1)
+	})
+}
 
-	rankingUnion(r1, r2)
-	res2 := userChannelRank{
-		"user1": {
-			"channel1": 100,
-			"channel2": 200,
-		},
-		"user2": {
-			"channel1": 200,
-			"channel2": 400,
-		},
-	}
-	assert.Equal(res2, r1)
+func TestGetRankingsSinceForChannel(t *testing.T) {
+
 }
