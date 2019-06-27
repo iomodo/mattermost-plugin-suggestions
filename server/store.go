711228d3c42099a4eb3ce1673b2f0f1027fa8134
iomodo <wineson@gmail.com>
2019-06-27 13:41:50 +0400

    Refactor store

--- server/store.go
+++ server/store.go
@@ -15,71 +15,33 @@ type recommendedChannel struct {
 }
 
 func (p *Plugin) saveUserRecommendations(userID string, channels []recommendedChannel) error {
-	j, err := json.Marshal(channels)
-
-	if err != nil {
-		p.API.LogError("Can't marshal recommendations", "err", err.Error())
-		return err
-	}
-	if err := p.API.KVSet(userID, j); err != nil {
-		p.API.LogError("Can't set recommendations"+err.Error(), "err", err.Error()) //TODO
-		return err
-	}
-	return nil
+	return p.save(userID, channels)
 }
 
 func (p *Plugin) retreiveUserRecomendations(userID string) ([]*recommendedChannel, error) {
-	j, err := p.API.KVGet(userID)
-	if err != nil {
-		p.API.LogError("can't get recommendations"+err.Error(), "err", err.Error()) //TODO
-		return nil, err
-	}
 	recommendations := make([]*recommendedChannel, 0)
-	if err := json.Unmarshal(j, &recommendations); err != nil {
-		p.API.LogError("failed to unmarshal recommendations", "err", err.Error())
-		return nil, err
-	}
-	return recommendations, nil
+	err := p.retreive(userID, &recommendations)
+	return recommendations, err
 }
 
 func (p *Plugin) saveTimestamp(time int64) error {
-	t, err := json.Marshal(time)
-	if err != nil {
-		p.API.LogError("Can't marshal time", "err", err.Error())
-		return err
-	}
-	if err := p.API.KVSet(timestampKey, t); err != nil {
-		p.API.LogError("Can't set timestamp"+err.Error(), "err", err.Error()) //TODO
-		return err
-	}
-	return nil
+	return p.save(timestampKey, time)
 }
 
 func (p *Plugin) retreiveTimestamp() (int64, error) {
-	t, err := p.API.KVGet(timestampKey)
-	if err != nil {
-		p.API.LogError("can't get timestamp"+err.Error(), "err", err.Error()) //TODO
-		return 0, err
-	}
 	var time int64
-	if err := json.Unmarshal(t, &time); err != nil {
-		p.API.LogError("failed to unmarshal timestamp", "err", err.Error())
-		return 0, err
-	}
-	return time, nil
+	err := p.retreive(timestampKey, &time)
+	return time, err
 }
 
 func (p *Plugin) saveUserChannelRanks(ranks userChannelRank) error {
-	r, err := json.Marshal(ranks)
-	if err != nil {
-		p.API.LogError("Can't marshal time", "err", err.Error())
-		return err
-	}
-	if err := p.API.KVSet(userChannelRanksKey, r); err != nil {
-		p.API.LogError("Can't set timestamp"+err.Error(), "err", err.Error()) //TODO
-		return err
-	}
-	return nil
+	return p.save(userChannelRanksKey, ranks)
+}
+
+func (p *Plugin) retreiveUserChannelRanks() (userChannelRank, error) {
+	var ranks userChannelRank
+	err := p.retreive(userChannelRanksKey, &ranks)
+	return ranks, err
 }
 
 func (p *Plugin) save(key string, value interface{}) error {
@@ -99,19 +61,3 @@ func (p *Plugin) retreive(key string, value interface{}) error {
 	}
 	return json.Unmarshal(v, value)
 }
-
-/*
-func (p *Plugin) retreiveTimestamp() (int64, error) {
-	t, err := p.API.KVGet(timestampKey)
-	if err != nil {
-		p.API.LogError("can't get timestamp"+err.Error(), "err", err.Error()) //TODO
-		return 0, err
-	}
-	var time int64
-	if err := json.Unmarshal(t, &time); err != nil {
-		p.API.LogError("failed to unmarshal timestamp", "err", err.Error())
-		return 0, err
-	}
-	return time, nil
-}
-*/
